c58e6c50316e6114d49857b044053b6a13dd4d19
John Doe <hamedhasan422421@gmail.com>
2022-09-19 17:39:59 +0300

    implement description price image status schemas fields with validate & enum & timestemps

--- models/tour.js
+++ models/tour.js
@@ -9,5 +9,45 @@ const tourSchema = mongoose.Schema({
         unique: [true, 'Name must be unique'],
         minLength: [3, 'Name must be at least 3 characters'],
         maxLength: [100, 'Name is too large'],
+    },
+    description: {
+        type: String,
+        required: true,
+        min: [3, 'description must be at least 3 characters']
+    },
+    price: {
+        type: Number,
+        required: true,
+        unique: [true, 'Price must be unique'],
+        min: [0, 'price can"t be negative'],
+        validate: {
+            validator: (values) => {
+                const isInteger = Number.isInteger(values);
+                if(isInteger) {
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        },
+        message: 'Price must be an integer'
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: {
+            values: ['Independent-Tours','Hosted-Tours','Freedom-Tours'],
+            message: 'status must be values'
+        }
     }
+}, {
+    timestamps: true,
 })
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
